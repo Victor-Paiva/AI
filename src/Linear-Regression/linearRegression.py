d6b3fc71717a81024d5ba2b2bc723b2a278ad3f8
Victor-Paiva <69825633+Victor-Paiva@users.noreply.github.com>
2021-01-11 19:22:15 -0300

    Add the final features to Linear-Regression

--- src/Linear-Regression/linearRegression.py
+++ src/Linear-Regression/linearRegression.py
@@ -6,5 +6,21 @@ class LinearRegression:
         self.labels = labels
         self.weights = np.random.normal(size=2)
 
-    def fit(epochs=10, lr=0.1):
-        pass
+    def predict(self, point):
+        """Returns a prediction using the given point"""
+        return self.weights[0] + self.weights[1] * point
+
+    def fit(self, epochs=30, lr=0.01):
+        """Trains the model (30 epochs by default) using Gradient Descent"""
+        for epoch in range(epochs):
+            print('epoch', epoch+1)
+
+            # Make the summations
+            s_0, s_1 = 0, 0
+            for i in range(self.features.shape[0]):
+                s_0 += self.features[i] @ self.weights.T - self.labels[i]
+                s_1 += (self.features[i] @ self.weights.T - self.labels[i])
+
+            # Update the weights
+            self.weights[0] -= lr * (1/self.features.shape[0]) * s_0
+            self.weights[1] -= lr * (1/self.features.shape[0]) * s_1
--- src/Linear-Regression/main.py
+++ src/Linear-Regression/main.py
@@ -1,17 +1,41 @@
 import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 from linearRegression import LinearRegression
 
-def generate_samples(coef, lims=(0, 50),  size=100):
+mpl.style.use(['ggplot'])
+
+def generate_samples(coef, lims=(0, 50),  size=100, noise=True):
+    """Generates sample data using the given coefficients and adds some noise to it (optional)"""
     feat = np.linspace(lims[0], lims[1], size).reshape((size, 1))
-    labs = feat * coef[0] + coef[1] + np.random.normal(size=feat.shape) / 10
+    labs = feat * coef[1] + coef[0]
+    if noise:
+        labs += np.random.normal(size=feat.shape) * 10
     return feat, labs
+
+def show(x1, y1, x2, y2):
+    """Plots the data points and the regressor"""
+    plt.scatter(x1, y1, label='Data Points', color='steelblue')
+    plt.plot(x2, y2, label='Regressor', color='red')
+    plt.title('Linear Regression')
+    plt.xlabel('$x$')
+    plt.ylabel('$y$')
+    plt.legend(fancybox=True)
+    plt.savefig('regressor.png')
 
 def main():
     # Generate the samples
-    weights = np.random.uniform(low=-2, high=2, size=2)
+    weights = np.random.uniform(low=-5, high=5, size=2)
     features, labels = generate_samples(weights)
 
+    # Create and train the regressor
     reg = LinearRegression(features, labels)
+    reg.fit()
+
+    # Show the results
+    x, y = generate_samples(reg.weights, noise=False)
+    show(features, labels, x, y)
+
 
 if __name__ == '__main__':
     main()
